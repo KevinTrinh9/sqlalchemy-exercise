1fcb4df76dc3da0576d4c24ec89b875db5bcfa85
Kevin Trinh <kevintrinh@Computer.local>
2023-01-11 19:46:58 -0800

    add posts

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -11,11 +12,27 @@ class User(db.Model):
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
-
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
     @property
     def full_name(self):
         """Returns full name of user."""
         return f"{self.first_name} {self.last_name}"
+
+class Post(db.Model):
+    """Blog post."""
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    @property
+    def friendly_date(self):
+        """Returns formatted date."""
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
 def connect_db(app):
     """Connects this database to provided Flask app."""
